8a32782d1259f7a529117846efda7f5391d07552
Zahidul Haque <zahidul.haque767@gmail.com>
2024-06-28 06:04:33 +0600

    feat: add user model

---
+++ models/user.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    authorId: String,
+    fullName: String,
+    username: {
+      type: String,
+      unique: true,
+      lowercase: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    password: String,
+    phone: String,
+    avatar: String,
+    role: String,
+    credit: Number,
+    balance: Number,
+    googleId: String,
+    properties: [
+      { type: mongoose.Schema.Types.ObjectId, ref: "ListingProperty" },
+    ],
+    isEmailVerified: {
+      type: Boolean,
+      default: false,
+    },
+    isPhoneVerified: {
+      type: Boolean,
+      default: false,
+    },
+    isBanned: {
+      type: Boolean,
+      default: false,
+    },
+    address: {
+      street: String,
+      city: String,
+      state: String,
+      country: String,
+      zipCode: String,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
