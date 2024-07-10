02e1d38e5e996a0e07e5e46a0f63c7777b0d3be6
Jahid0178 <jahidulhaque767@gmail.com>
2024-07-11 01:13:46 +0600

    feat: add user analytics data total property total view property and most viewed property data

---
+++ controllers/analytics.controller.js
@@ -0,0 +1,55 @@
+const Property = require("../models/property.model");
+
+const getAnalyticsByUserId = async (req, res) => {
+  try {
+    const userId = req.user._id;
+
+    const properties = await Property.find({ author: userId });
+
+    const totalPropertyViews = properties.reduce((total, property) => {
+      return total + property.views;
+    }, 0);
+
+    if (!properties) {
+      return res
+        .status(404)
+        .json({ message: "Properties not found", status: 404 });
+    }
+
+    res.status(200).json({
+      message: "Properties fetched successfully",
+      status: 200,
+      count: properties.length,
+      data: {
+        properties,
+        totalPropertyViews,
+      },
+    });
+  } catch (error) {
+    console.log("property fetching analytics error", error);
+  }
+};
+
+const getAnalyticsMostViewedProperty = async (req, res) => {
+  try {
+    const mostViewdProperty = await Property.find()
+      .populate("images", "url")
+      .sort({ views: -1 });
+
+    if (!mostViewdProperty) {
+      return res.json({
+        message: "Most viewed property not found",
+        status: 404,
+      });
+    }
+
+    res.json({
+      message: "Most viewed property fetched successfully",
+      status: 200,
+      count: mostViewdProperty.length,
+      data: mostViewdProperty,
+    });
+  } catch (error) {}
+};
+
+module.exports = { getAnalyticsByUserId, getAnalyticsMostViewedProperty };
