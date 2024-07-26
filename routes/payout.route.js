07ba4b073ac28943c882a3882ad35025f8fbbea5
Jahid0178 <jahidulhaque767@gmail.com>
2024-07-27 02:06:37 +0600

    feat: add get payout by user id route

--- routes/payout.route.js
+++ routes/payout.route.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const {
   handlePayout,
   handleSuccessPayout,
+  getPayoutByUserId,
 } = require("../controllers/payout.controller");
 
 router.post("/", handlePayout);
 router.post("/success/:tranId/:userId/:packageId", handleSuccessPayout);
+router.get("/get-payout/:userId", getPayoutByUserId);
 
 module.exports = router;
